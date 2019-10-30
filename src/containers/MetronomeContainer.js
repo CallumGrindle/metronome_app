ddb0db20967fd80ee8aa89b264eefc09e2fd7d0c
CallumGrindle <callumgrindle@gmail.com>
2019-10-30 12:19:13 +0000

    set up and initial beeping

--- src/containers/MetronomeContainer.js
+++ src/containers/MetronomeContainer.js
@@ -2,6 +2,15 @@ import React, {Component} from 'react';
 import Metronome from '../components/Metronome.js';
 
 class MetronomeContainer extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    sounds: [],
+    selectedSound: null,
+    interval: null
+  }
+}
+
   render() {
     return <Metronome />
   }
