073e488fb70069f9ab91add641b7a67e816e858a
CallumGrindle <callumgrindle@gmail.com>
2019-10-30 12:08:56 +0000

    metronome making beep

---
+++ src/containers/MetronomeContainer.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import Metronome from '../components/Metronome.js';
+
+class MetronomeContainer extends Component {
+  render() {
+    return <Metronome />
+  }
+}
+
+export default MetronomeContainer;
